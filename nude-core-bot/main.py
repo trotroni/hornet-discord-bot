5a34c1085e076ef9e00d2c84b7aa5691c3e62959
Mattoctet <114805974+Trotroni@users.noreply.github.com>
2026-01-16 13:35:39 +0100

    Add custom commands loading functionality

--- nude-core-bot/main.py
+++ nude-core-bot/main.py
@@ -2,7 +2,7 @@
 from common.imports import *
 from common.init import create_bot
 from common.langManager import lang_manager
-from common.utils import command_log, date_now, send_with_warning, get_cpu_temperature, cpu_temp_verification, custom_commands, save_custom_commands
+from common.utils import command_log, date_now, send_with_warning, get_cpu_temperature, cpu_temp_verification, custom_commands, save_custom_commands, load_custom_commands
 
 ### config
 bot, CONFIG, logger = create_bot("core")
