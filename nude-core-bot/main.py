43036613ebfb9b6b270f9eebbb37e83a512adb6b
trotroni <breton.matthias@outlook.com>
2026-01-16 23:55:13 +0100

    Update main.py

--- nude-core-bot/main.py
+++ nude-core-bot/main.py
@@ -23,6 +23,23 @@ async def on_ready():
         return
 
     logger.info("✅ Core bot prêt")
+    
+    channel_id = CONFIG["GENERAL_CHANNEL_ID"]
+    channel = bot.get_channel(channel_id)
+
+    if channel is None:
+        print("❌ Channel général introuvable")
+        return
+
+    embed = discord.Embed(
+        title="C'est bon !",
+        description="J'en ai marre !",
+        color=discord.Color.green()
+    )
+    embed.timestamp = discord.utils.utcnow()
+
+    await channel.send(embed=embed)
+    
 
 # config translation
 t = lang_manager.translation_key
