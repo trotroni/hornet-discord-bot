1f045119c88598f6712549017dd154e9a72f22fe
trotroni <breton.matthias@outlook.com>
2026-01-22 23:07:42 +0100

    Update main.py

--- nude-core-bot/main.py
+++ nude-core-bot/main.py
@@ -179,7 +179,7 @@ async def before_cpu_task():
 # ---------------- COMMANDES SLASH ----------------
 
 # /ping
-@bot.tree.command(name="ping", description=t("Teste la réactivité du bot"))
+@bot.tree.command(name="bierre", description=t("""Teste l'alcoolémie du bot"""))
 async def ping(interaction: discord.Interaction):
     command_log(interaction.command.name, interaction.user.id, interaction.user.name)
     await interaction.response.defer(ephemeral=CONFIG["EPHEMERAL_GLOBAL"])
